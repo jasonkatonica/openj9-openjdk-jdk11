63777d98d2b5f62411e1003f1f3a3edc7a7ebd2c
Jason Katonica <katonica@us.ibm.com>
2023-02-02 10:02:48 -0500

    Add SunPKCS11 provider to allowable list for jar verify

--- src/java.base/share/classes/sun/security/jca/Providers.java
+++ src/java.base/share/classes/sun/security/jca/Providers.java
@@ -88,6 +88,7 @@ public class Providers {
         // by EC algorithms. So it's still safe to be listed here.
         "SunEC",
         "SunJCE",
+        "SunPKCS11"
     };
 
     // Return Sun provider.
